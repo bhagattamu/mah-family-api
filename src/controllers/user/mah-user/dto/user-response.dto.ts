d1b1129eff1219e5eca486f882dbd8f381b204ac
Bhagat Gurung <bhagat@kilroyblockchain.com>
2021-01-17 21:28:30 +0545

    Removed unused variable

--- src/controllers/user/mah-user/dto/user-response.dto.ts
+++ src/controllers/user/mah-user/dto/user-response.dto.ts
@@ -1,5 +1,4 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsArray, IsEmail, IsNotEmpty, IsString, MinLength } from 'class-validator';
 
 export class CreateUserResponse {
     @ApiProperty({
